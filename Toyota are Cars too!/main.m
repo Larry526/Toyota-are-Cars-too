f1ee5eb328d57430b89d964f65ea167e7638e09c
Larry Luk <larryluk526@gmail.com>
2017-10-09 19:00:27 -0400

    Initial Commit

---
+++ Toyota are Cars too!/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Toyota are Cars too!
+//
+//  Created by Larry Luk on 2017-10-09.
+//  Copyright © 2017 App App Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
